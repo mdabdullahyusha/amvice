95774eb69c46f01cd00b8e87ff8ccb1b066fbb3a
mdabdullahyusha <emonyusha14@gmail.com>
2022-11-08 18:16:30 +0600

    Update 1 before submit

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,17 @@ $(document).ready(function(){
     $('.nn').click(function(){
         $('.collapse_dropdown').toggle();
     });
+    $('.back_to_top').click(function(){
+        $('html, body').animate({scrollTop:0}, 500);
+    });
+    $(window).on('scroll', function(){
+        var scroller = $(this).scrollTop();
+        if (scroller > 400) {
+            $('.back_to_top').show(500);
+        } else {
+            $('.back_to_top').hide(500);
+        }
+    });
 });
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 3,
